cf345241929b39991695d1f17252ae6c6f4f2dfa
Carmen Rand <carmenrand@MacBook-Pro-5.local>
2025-12-04 09:43:12 +0200

    Refactor to ES modules

---
+++ game.js
@@ -0,0 +1,54 @@
+// Game State
+export let game = {
+    muffins: 0,
+    totalMuffins: 0,
+    mps: 0,
+    startTime: Date.now(),
+    upgrades: {
+        cursor: {
+            id: 'cursor',
+            name: 'Pink Cursor',
+            cost: 15,
+            baseCost: 15,
+            mps: 0.1,
+            count: 0,
+            icon: ''
+        },
+        grandma: {
+            id: 'grandma',
+            name: 'Home Kitchen',
+            cost: 100,
+            baseCost: 100,
+            mps: 1,
+            count: 0,
+            icon: 'assets/grandma.png'
+        },
+        farm: {
+            id: 'farm',
+            name: 'Muffin Stand',
+            cost: 1100,
+            baseCost: 1100,
+            mps: 8,
+            count: 0,
+            icon: 'assets/stand.png'
+        },
+        bakery: {
+            id: 'bakery',
+            name: 'Corner Bakery',
+            cost: 12000,
+            baseCost: 12000,
+            mps: 47,
+            count: 0,
+            icon: 'assets/bakery.png'
+        },
+        factory: {
+            id: 'factory',
+            name: 'Fancy Patisserie',
+            cost: 130000,
+            baseCost: 130000,
+            mps: 260,
+            count: 0,
+            icon: 'assets/patisserie.png'
+        }
+    }
+};
